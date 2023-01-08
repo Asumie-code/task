6592b908de7d61f7909be2670d827d1e24ed07e8
Asumie-code <darkeangel.1998.ia@gmail.com>
2023-01-08 19:02:24 +0100

    chore: test deployment

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
   return (
     <div className="App">
       <h1>repo for coding tasks</h1>
-      <p>this page is for coding tasks requested by clients. </p> 
+      <p>this page is for coding tasks requested by clients. with automatic deploymenet of tasks </p> 
     </div>
   );
 }
